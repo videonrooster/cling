9098cbb4223be1728c16ddedd047ea8731098957
Michael Pujos <bubbleguuum@free.fr>
2012-01-01 20:39:05 +0100

    recover non XML Encoded LastChange (Orange liveradio)

--- core/src/main/java/org/fourthline/cling/transport/impl/RecoverGENAEventProcessor.java
+++ core/src/main/java/org/fourthline/cling/transport/impl/RecoverGENAEventProcessor.java
@@ -21,6 +21,7 @@ import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.fourthline.cling.model.message.UpnpMessage;
 import org.fourthline.cling.model.message.gena.IncomingEventRequestMessage;
@@ -36,19 +37,19 @@ import org.seamless.xml.XmlPullParserUtils;
  * @author Michael Pujos
  */
 public class RecoverGENAEventProcessor extends PullGENAEventProcessor {
-	
+
 	private static Logger log = Logger.getLogger(RecoverGENAEventProcessor.class.getName());
 
 	public void readBody(IncomingEventRequestMessage requestMessage) throws UnsupportedDataException {
 
 		checkRequestBodyValidity(requestMessage);
-		
+
 		try {
 			super.readBody(requestMessage);
 		} catch(UnsupportedDataException ex) {
-			
+
 			log.severe("bad GENA Event XML found: " + ex);
-			
+
 			// some properties may have been read by the pull parser at this point, so reset the list
 			requestMessage.getStateVariableValues().clear();
 
@@ -59,7 +60,7 @@ public class RecoverGENAEventProcessor extends PullGENAEventProcessor {
 				super.readBody(requestMessage);
 				log.info("sucessfully fixed bad GENA XML");
 			} catch(UnsupportedDataException ex2) {
-				
+
 				// check if some properties were read
 				if(requestMessage.getStateVariableValues().isEmpty()) {
 					// throw the initial exception containing unmodified xml
@@ -67,11 +68,11 @@ public class RecoverGENAEventProcessor extends PullGENAEventProcessor {
 				}
 
 				log.warning("partial read of GENA event properties (probably due to truncated XML)");
-				
+
 			}
 		}
 	}
-	
+
 	private String fixXMLEncodedLastChange(String xml) {
 		Pattern pattern = Pattern.compile("<LastChange>(.*)</LastChange>", Pattern.DOTALL);
 		Matcher matcher = pattern.matcher(xml);
@@ -80,23 +81,35 @@ public class RecoverGENAEventProcessor extends PullGENAEventProcessor {
 
 			String xmlEncodedLastChange = matcher.group(1);
 
-			// delete funky characters (at least found in the Philips NP2900 that inserts garbage HTML)
-			String fixedXmlEncodedLastChange = StringUtils.replaceChars(xmlEncodedLastChange, "<>", null);
-			if(fixedXmlEncodedLastChange.equals(xmlEncodedLastChange)) {
-				return xml;
-			}
-			
-			log.warning("deleted invalid characters in LastChange");
+			if(XmlPullParserUtils.isNullOrEmpty(xmlEncodedLastChange)) return xml;
 
-			String fixedXml =
-					"<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-							"<e:propertyset xmlns:e=\"urn:schemas-upnp-org:event-1-0\">" +
-							"<e:property>" +
-							"<LastChange>" +
-							fixedXmlEncodedLastChange +
-							"</LastChange>" +
-							"</e:property>" +
-							"</e:propertyset>";
+			xmlEncodedLastChange = xmlEncodedLastChange.trim();
+			
+			String fixedXmlEncodedLastChange;
+			
+			// first look if LastChange text is XML encoded (some renderers will sent it not XML encoded)
+			if(xmlEncodedLastChange.charAt(0) == '<') {
+				fixedXmlEncodedLastChange = StringEscapeUtils.escapeXml(xmlEncodedLastChange);
+				log.warning("fixed LastChange that was not XML encoded");
+			}  else {
+				// delete potential funky characters (at least found in the Philips NP2900 that inserts garbage HTML)
+				fixedXmlEncodedLastChange = StringUtils.replaceChars(xmlEncodedLastChange, "<>", null);
+				if(fixedXmlEncodedLastChange.equals(xmlEncodedLastChange)) {
+					// no change
+					return xml;
+				}
+
+				log.warning("deleted invalid characters in LastChange");
+			}
+
+			String fixedXml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+			"<e:propertyset xmlns:e=\"urn:schemas-upnp-org:event-1-0\">" +
+			"<e:property>" +
+			"<LastChange>" +
+			fixedXmlEncodedLastChange +
+			"</LastChange>" +
+			"</e:property>" +
+			"</e:propertyset>";
 
 			return fixedXml;
 
